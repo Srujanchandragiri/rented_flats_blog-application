b8232cab98fc23c565589fbb3fbacf8c09223f45
Srujanchandragiri <145650122+Srujanchandragiri@users.noreply.github.com>
2023-12-18 16:00:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,59 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+//const port = 6000;
+
+mongoose.connect('mongodb+srv://Srujan:Srujanc12@atlascluster.gjrriix.mongodb.net/rentalapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+ 
+app.use(cors());
 
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Connected to MongoDB');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+const UserSchema = mongoose.Schema({
+    title: String,
+    date: String,
+    rent: String,
+    content: String,
+    image: String,
+    link: String,
+
+    
+});
+
+const UserModel = mongoose.model('user', UserSchema);
+app.get('/getUser', (req, res) => {
+    UserModel.find({})
+        .then(function (user) {
+            console.log(user); // Log the retrieved data
+            res.json(user);
+        })
+        .catch(function (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        });
+});
+/*app.get('/getUser', (req, res) => {
+    UserModel.find({})
+        .then(function (users) {
+            res.json(users);
+        })
+        .catch(function (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        });
+});*/
+
+app.listen(3000, () => {
+    console.log("Server is Running");
 });
